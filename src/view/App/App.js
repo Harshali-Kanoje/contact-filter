c6efe53e4bebd11fbed185b4feddb7686a7347b8
Harshali Kanoje <harshalikanoje@gmail.com>
2023-09-24 16:04:22 -0700

    list added with search bar

--- src/view/App/App.js
+++ src/view/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 // import {data} from './../../data/phonebook'
 import {data} from './../../data/phonebook'
 import Contact from '../../component/Contact/Contact'
@@ -6,15 +6,35 @@ import Contact from '../../component/Contact/Contact'
 const App = () => {
   console.log(data)
   const [contact ,setContact] = useState(data)
+  const [searchterm ,setSearchterm] = useState('')
+
+  useEffect(() =>{
+    const filerdata = data.filter((element,index) => {
+      const name = element.name.toLowerCase()
+      const number = element.number.toString()
+      const quety = searchterm.toLowerCase()
+      return (name.includes(quety) || number.includes(quety))
+      
+     })
+     setContact(filerdata)
+  },[searchterm])
+  
   return (
+    <>
+    <input type='text' placeholder='search your contacts' value={searchterm} onChange={(e) => {
+      setSearchterm(e.target.value)
+      // console.log(e)
+    }}/>
     <div>
       {
-        data.map((element,index) => {
+        contact.map((element,index) => {
           const {name,number} = element;
           return <Contact name={name} number={number} key={index}/>
         })
       }
     </div>
+    
+  </>
   )
 }
 
