b6ba4bf1bf234140c63b9f6da4c649331b8f33ed
Harshali Kanoje <harshalikanoje@gmail.com>
2023-10-07 08:25:53 -0700

    more contacts added

--- src/data/phonebook.js
+++ src/data/phonebook.js
@@ -1,18 +1,54 @@
 export const data = [
     {
-      name : "rani",
+      name : "Rani",
       number : 8677658768875
     },
     {
-      name : "chandni",
+      name : "Chandni",
       number : 87658768875
     },
     {
-      name : "samiksha",
+      name : "Samiksha",
       number : 9788648687687
     },
     {
-      name :" harshali",
+      name :" Harshali",
+      number : 9877868698688
+    }
+    ,{
+      name :" Mummy",
+      number : 9877868698688
+    },
+    {
+      name :" Pintu Bro",
+      number : 9877868698688
+    },
+    {
+      name :" Monu Sandel",
+      number : 9877868698688
+    },
+    {
+      name :" Sonu Jangde",
+      number : 9877868698688
+    },
+    {
+      name :" Sunita",
+      number : 9877868698688
+    },
+    {
+      name :" Pooja Ukey",
+      number : 9877868698688
+    },
+    {
+      name :" Golu Malviya",
+      number : 9877868698688
+    },
+    {
+      name :" Sandip Sandel",
+      number : 9877868698688
+    },
+    {
+      name :" Pradip Sandel",
       number : 9877868698688
     }
   ]
